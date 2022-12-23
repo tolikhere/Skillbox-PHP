2e746e87610adbf33efd216a9685d2678d2cb807
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2022-12-23 13:05:53 +0300

    Apply getters and setters for "Telegraph"

---
+++ module-1/homework-11/app.php
@@ -0,0 +1,8 @@
+
+<?php
+
+require_once __DIR__ . '/TelegraphText.php';
+require_once __DIR__ . '/FileStorage.php';
+
+use Dpo\Php\Developer\Base\Module11\TelegraphText;
+use Dpo\Php\Developer\Base\Module11\FileStorage;
